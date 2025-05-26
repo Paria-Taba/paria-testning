3d3fd8daeacec7e31960f251b6b6181278a93416
paria <pariyataba@gmail.com>
2025-05-26 10:38:05 +0200

    get totalt cart value, edit cart, remove from cart

--- src/cart.js
+++ src/cart.js
@@ -1,13 +1,11 @@
 
-
 import { isProduct } from "./validation.js"
 
 
 //function getTotalCartValue()
-//function addToCart(newItem)
 //function removeFromCart(itemId)
 //function editCart(itemId, newValues)
-//function clearCart()
+
 
 let cart = []
 let idCounter = 2002
@@ -36,7 +34,31 @@ function addToCart(newItem) {
 	idCounter++
 	cart.push(cartItem)
 }
+function editCart(itemId, newValues){
+const item = cart.find(cartItem => cartItem.id === itemId)
+if (!item) return false
+
+if (newValues.amount !== undefined) {
+  item.amount = newValues.amount
+}
+  if (newValues.item !== undefined) {
+    item.item = { ...item.item, ...newValues.item }
+  }
+
+return true
+}
+
+function getTotalCartValue(){
+ return cart.reduce((sum, cartItem) => {
+    return sum + cartItem.amount * cartItem.item.price
+  }, 0)
+}
+function removeFromCart(itemId){
+	  const index = cart.findIndex(cartItem => cartItem.id === itemId)
+  if (index === -1) return false
+  cart.splice(index, 1)
+  return true
+}
 
 
-
-export { getCartItemCount, addToCart,clearCart,getItem }
+export { getCartItemCount, addToCart,clearCart,getItem, getTotalCartValue,editCart,removeFromCart}
--- src/tests/cart.test.js
+++ src/tests/cart.test.js
@@ -1,4 +1,4 @@
-import { addToCart, getCartItemCount,clearCart,getItem } from "../cart"
+import { addToCart, getCartItemCount,clearCart,getItem,getTotalCartValue,editCart,removeFromCart } from "../cart"
 
 describe('Cart', () => {
     beforeEach(() => {
@@ -57,7 +57,12 @@ test("getItem returnerar korrekt objekt från kundvagnen-2",()=>{
 
 })
 
-  
+
+})
+
+
+describe("test av addToCart(newItem)",()=>{
+	  
 test('addToCart lägger till en ny produkt i kundvagnen', () => {
         const itemCountBefore = getCartItemCount()
         const input = { id: 1002, name: 'Vattenpistol', price: 40 }
@@ -66,7 +71,94 @@ test('addToCart lägger till en ny produkt i kundvagnen', () => {
 
         expect(itemCountAfter).toBe(itemCountBefore + 1)
     })
+	test('antal av addToCart', () => {
+         clearCart()
+         addToCart({ id: 1002, name: 'Vattenpistol', price: 40 })
+		 addToCart({ id: 1003, name: 'anka', price: 50 })
+		 addToCart({ id: 1004, name: 'appa', price: 70 })
+         const expected=3
+        const itemCountAfter = getCartItemCount()
 
+        expect(itemCountAfter).toBe(expected)
+    })
 })
 
+describe("editCart(itemId,newValue)",()=>{
+	test("editCart uppdaterar amount korrekt",()=>{
+		clearCart()
+		const input={ id: 1001, name: "Badanka", price: 100 }
+		addToCart(input)
+		 const item = getItem(0)
+		 const actual = editCart(item.id, {amount: 3 })
+		 expect(actual).toBe(true)
+		 expect(item.amount).toBe(3)
+	
+	})
+		test("editCart byter korrekt namn av produkt",()=>{
+		clearCart()
+		const input={ id: 1001, name: "Badanka", price: 100 }
+		addToCart(input)
+		const item=getItem(0)
+		const actual=editCart(item.id,{item: { name: "Anka" }})
+		expect(actual).toBe(true)
+		expect(getItem(0).item.name).toBe("Anka")
+	
+	
+	})
+	test("editCart byter korrekt price av produkt",()=>{
+		clearCart()
+		const input={ id: 1001, name: "Badanka", price: 100 }
+		addToCart(input)
+		const item=getItem(0)
+		const actual=editCart(item.id,{item: { price: 150 }})
+		expect(actual).toBe(true)
+		expect(getItem(0).item.price).toBe(150)
+	
+	
+	})
+})
 
+describe("get total cart value",()=>{
+test("getTotalCartValue returnerar korrekt summa av produkternas pris",()=>{
+	clearCart()
+    addToCart({ id: 1001, name: 'Boll', price: 100 }) 
+    addToCart({ id: 1002, name: 'Djur', price: 50 })  
+const expected=100 + 50;
+const actual=getTotalCartValue()
+expect(actual).toBe(expected)
+
+})
+test("getTotalCartValue med olika amount för produkter", () => {
+  clearCart()
+
+  const inputA = { id: 1000, name: "Anka", price: 150 }
+  const inputB = { id: 1001, name: "Appa", price: 50 }
+
+  addToCart(inputA)
+  addToCart(inputB)
+
+  editCart(getItem(0).id, { amount: 1 })  
+  editCart(getItem(1).id, { amount: 2 }) 
+
+  const expected = 150 + 2*50
+  const actual = getTotalCartValue()
+
+  expect(actual).toBe(expected)
+})
+
+})
+describe("removeFromCart",()=>{
+	test("kan tabort produkt from kart",()=>{
+		clearCart(),
+		addToCart({id: 1000, name: "Anka", price: 150})
+		addToCart({id: 1001, name: "Appa", price: 50} )
+		addToCart({id: 1002, name: "apelsin", price: 60} )
+		const actual=removeFromCart(getItem(1).id)
+		expect(actual).toBe(true)
+	 const countAfterRemoval = getCartItemCount()
+         expect(countAfterRemoval).toBe(2) 
+	})
+	  test("removeFromCart returnerar false om id saknas", () => {
+    expect(removeFromCart(9999)).toBe(true)
+  })
+})
